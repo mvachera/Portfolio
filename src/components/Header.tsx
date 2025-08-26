ea800872f4e29fee63980fcbf6e1def66964d088
mvachera <mvachera@student.42.fr>
2025-08-26 18:04:41 +0200

    header upgraded

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Home, HelpCircle, Mail, Rocket } from 'lucide-react';
 
 function Header() {
 	return (
-		<nav className="bg-green-100 w-full flex justify-center space-x-6">
-			<Link to="/">Accueil</Link>
-			<Link to="/About">About</Link>
-			<Link to="/Contact">Contact</Link>
-			<Link to="/Projects">Projects</Link>
+		<nav className="fixed top-2 flex justify-center space-x-6 px-6 py-3">
+			<Link to="/" className='flex items-center space-x-1'>
+				<Home size={16} className='text-blue-500' />
+				<span className='text-white'>Home</span>
+			</Link>
+			<Link to="/About" className='flex items-center space-x-1'>
+				<HelpCircle size={16} className='text-green-500' />
+				<span className='text-white'>About</span>
+			</Link>
+			<Link to="/Contact" className='flex items-center space-x-1'>
+				<Mail size={16} className='text-yellow-500' />
+				<span className='text-white'>Contact</span>
+			</Link>
+			<Link to="/Projects" className='flex items-center space-x-1'>
+				<Rocket size={16} className='text-purple-500' />
+				<span className='text-white'>Projects</span>
+			</Link>
 		</nav>
 	)
 }
--- src/pages/Accueil.tsx
+++ src/pages/Accueil.tsx
@@ -1,14 +1,21 @@
 import Header from '../components/Header';
+import reactLogo from '../assets/logo.svg';
 
 function Accueil() {
 	return (
 		<div>
-			<Header />
-			<div className="bg-blue-100 min-h-screen flex flex-col justify-center items-center">
-				<h1 className="text-4xl font-bold text-gray-800">Mathéo Vacherat</h1>
-				<h2 className="text-4xl text-gray-600 mt-2">Développeur Full-Stack</h2>
-				<p className="text-lg text-gray-600 mt-4 text-center max-w-2xl">"Étudiant développeur 
-					Full-Stack à l'École 42, passionné par le développement web et les technologies modernes"</p>
+			<div className="bg-black min-h-screen flex flex-col justify-center items-center bg-cover bg-center">
+				<img 
+        			src={reactLogo} 
+        			alt="background logo"
+        			className="absolute w-98 h-96 opacity-20 animate-spin"
+        			style={{ animationDuration: '20s' }}
+      			/>
+				<Header />
+				<h1 className="text-4xl font-bold text-white">Mathéo Vacherat</h1>
+				<h2 className="text-4xl text-white mt-2">Développeur Full-Stack</h2>
+				<p className="text-lg text-white mt-4 text-center max-w-2xl">Étudiant développeur 
+					Full-Stack à l'École 42, passionné par le développement web et les technologies modernes.</p>
 			</div>
 		</div>
 	)
