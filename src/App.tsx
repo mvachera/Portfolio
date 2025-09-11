933c6d5b97a2bfb11f5ef743d88380494eb54767
mvachera <mvachera@student.42.fr>
2025-09-11 14:34:33 +0200

    Fix React Router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Toaster } from "@/components/ui/toaster";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Portfolio">
       <Routes>
         <Route path="/" element={<Accueil />} />
       </Routes>
